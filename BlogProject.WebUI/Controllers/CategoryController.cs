559742b543843704f79c18600ed4d0c06dc31419
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-01-19 00:36:25 +0300

    Kategori Listeleme

--- BlogProject.WebUI/Controllers/CategoryController.cs
+++ BlogProject.WebUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogProject.BusinessLayer.Conrate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,19 @@ namespace BlogProject.WebUI.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager();
+
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAll();
+
+            return View(categoryValues);
         }
     }
 }
