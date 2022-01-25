b223c1d80a01b1a85cd43288bc0cd910c36d63cc
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2022-01-26 01:03:06 +0300

    Sidebar İle Beraber Kategori İşlemleri

---
+++ BlogProject.WebUI/Controllers/AdminCategoryController.cs
@@ -0,0 +1,55 @@
+﻿using BlogProject.BusinessLayer.Conrate;
+using BlogProject.BusinessLayer.ValidationRule;
+using BlogProject.DataAccessLayer.EntityFramework;
+using BlogProject.EntityLayer.Concrate;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogProject.WebUI.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryDAL());
+
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categoryVlues = categoryManager.GetList();
+
+            return View(categoryVlues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(category);
+
+            if (results.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+    }
+}
